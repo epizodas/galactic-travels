45dff05569ca4b9236d308a3b0b8cd23453ffe9d
ajud123 <audriusbertasius1@gmail.com>
2026-05-21 17:04:49 +0300

    Add calendar picker

---
+++ gdproj/Views/RouteView.gd
@@ -0,0 +1,5 @@
+extends MarginContainer
+class_name RouteView
+
+func submitEndDate(panel: DatePickerPanel, date):
+	print(date)
---
+++ gdproj/addons/DatePickerFiles/TestingScenes/TestScene.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+@onready var date_picker_panel = %DatePickerPanel
+
+func _on_test_button_pressed():
+	print(date_picker_panel.get_date_data())
+	
+func scream(panel: DatePickerPanel, date):
+	print(panel, date)
+	
